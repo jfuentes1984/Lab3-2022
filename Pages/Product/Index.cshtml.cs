2767a913f5b930fcfb8ab66e96c45b4c493207d8
jfuentes1984 <6179215@cegep-heritage.qc.ca>
2022-03-29 15:07:53 -0400

    Auth action added to products and users page - lab 4

--- Pages/Product/Index.cshtml.cs
+++ Pages/Product/Index.cshtml.cs
@@ -7,23 +7,44 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Lab3_2022.Model;
+using System.Security.Claims;
 
-namespace Lab3_2022.Pages.Product
+namespace Lab3_2022.Pages_Product;
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly ILogger<IndexModel> _logger;
+    private readonly DBContext _context;
+    [BindProperty]
+    public User? UserInfo { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger, DBContext context)
     {
-        private readonly DBContext _context;
+        _logger = logger;
+        _context = context;
+    }
+    public string? UserEmail { get; set; }
 
-        public IndexModel(DBContext context)
+    public IList<Lab3_2022.Model.Product> Product { get; set; }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        Product = await _context.Product.ToListAsync();
+        if (User.Identity != null)
         {
-            _context = context;
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            if (claimsIdentity.IsAuthenticated)
+            {
+                var email = claimsIdentity.FindFirst(ClaimTypes.Email);
+                if (email != null)
+                {
+                    UserEmail = email.Value;
+                    _logger.Log(LogLevel.Information, email.Value);
+                }
+            }
         }
 
-        public IList<Lab3_2022.Model.Product> Product { get; set; }
-
-        public async Task OnGetAsync()
-        {
-            Product = await _context.Product.ToListAsync();
-        }
+        return Page();
     }
 }
+
+
--- Pages/User/Index.cshtml.cs
+++ Pages/User/Index.cshtml.cs
@@ -2,42 +2,61 @@ using Lab3_2022.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
-namespace Lab3_2022.Pages_User
+namespace Lab3_2022.Pages_User;
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly ILogger<IndexModel> _logger;
+    private readonly DBContext _context;
+    [BindProperty]
+    public User? UserInfo { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger, DBContext context)
     {
-        private readonly ILogger<IndexModel> _logger;
-        private readonly DBContext _context;
-        [BindProperty]
-        public User? UserInfo { get; set; }
-
-        public IndexModel(ILogger<IndexModel> logger, DBContext context)
-        {
-            _logger = logger;
-            _context = context;
-        }
-        public async Task<IActionResult> OnGetAsync()
-        {
-            UserInfo = await _context.User.FirstOrDefaultAsync();
-
-            return Page();
-        }
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (UserInfo != null)
-            {
-                if (!ModelState.IsValid)
-                {
-                    return Page();
-                }
-                _context.Attach(UserInfo).State = EntityState.Modified;
-                await _context.SaveChangesAsync();
-            }
-
-
-            _logger.Log(LogLevel.Information, UserInfo.Name);
-            return RedirectToPage("../Index");
-        }
+        _logger = logger;
+        _context = context;
     }
+    public string? UserEmail { get; set; }
+    public async Task<IActionResult> OnGetAsync()
+    {
+        UserInfo = await _context.User.FirstOrDefaultAsync();
+        if (User.Identity != null)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            if (claimsIdentity.IsAuthenticated)
+            {
+                var email = claimsIdentity.FindFirst(ClaimTypes.Email);
+                if (email != null)
+                {
+                    UserEmail = email.Value;
+                    _logger.Log(LogLevel.Information, email.Value);
+                }
+                // foreach (var cl in claimsIdentity.Claims)
+                // {
+                //     _logger.Log(LogLevel.Information, $"{cl.Type}:{cl.Value}");
+                // }
+            }
+        }
+
+        return Page();
+    }
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (UserInfo != null)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _context.Attach(UserInfo).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+
+        _logger.Log(LogLevel.Information, UserInfo.Name);
+        return RedirectToPage("../Index");
+    }
+
 }
+
